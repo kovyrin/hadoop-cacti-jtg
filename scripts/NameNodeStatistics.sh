792f3c0ad66043a0214cdc483ac47c13aa02762a
ecapriolo <ecapriolo@d48a9152-7e18-4f87-800c-ca59c26a89d2>
2009-05-06 15:08:00 +0000

    Base Shell Scripts
    git-svn-id: http://www.jointhegrid.com/svn/hadoop-cacti-jtg/trunk@4 d48a9152-7e18-4f87-800c-ca59c26a89d2

---
+++ scripts/NameNodeStatistics.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ -z "$CACTI_HOME" ] ; then
+  echo "CACTI_HOME is not defined"
+  exit 1
+fi
+
+JTG_CLASSPATH="${CACTI_HOME}/scripts/hadoop-cacti-jtg.jar"
+JTG_CLASSNAME="com.jointhegrid.hadoopjmx.NameNodeStatistics"
+
+jmxURL=$1
+user=$2
+pass=$3
+java -cp ${JTG_CLASSPATH} ${JTG_CLASSNAME} $jmxURL $user $pass
