a1e8a5913fa67158679b4d1708b8ca397100f641
ecapriolo <ecapriolo@d48a9152-7e18-4f87-800c-ca59c26a89d2>
2009-11-23 21:59:24 +0000

    19 scripts
    git-svn-id: http://www.jointhegrid.com/svn/hadoop-cacti-jtg/trunk@57 d48a9152-7e18-4f87-800c-ca59c26a89d2

---
+++ scripts/0_19/jobclient/JobClient.sh
@@ -0,0 +1,26 @@
+JTG_CLASSPATH="`dirname $0`/../../hadoop-cacti-jtg.jar"
+JTG_CLASSNAME="com.jointhegrid.hadoopjmx.jobclient.JobClientFactory"
+
+HADOOP_VERSION="0_19"
+#change these to point at your hadoop path
+HADOOP_CORE="`dirname $0`/../../hadoop-0.19/hadoop-0.19-core.jar"
+HADOOP_EXTRA_LIBS="`dirname $0`/../../hadoop-0.19/lib"
+HADOOP_CONF=$1
+
+if [ ! -f $HADOOP_CORE ] ; then
+  echo "$HADOOP_CORE not found"
+  exit 1
+fi
+
+if [ ! -d $HADOOP_EXTRA_LIBS ] ; then
+  echo "$HADOOP_EXTRA_LIBS not found"
+  exit 2
+fi
+
+for f in $HADOOP_EXTRA_LIBS/* ; do
+  JTG_CLASSPATH=$f:$JTG_CLASSPATH
+done
+
+JTG_CLASSPATH=$HADOOP_CORE:$JTG_CLASSPATH
+
+java -cp ${JTG_CLASSPATH} ${JTG_CLASSNAME} $HADOOP_VERSION "$@"
